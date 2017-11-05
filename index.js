ae36fe6265b618786c6455ce36ecb00b0d7244ad
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-05 21:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
 
-"Hello, " + specialguest + "!"
+greetSpecialGuest
+
